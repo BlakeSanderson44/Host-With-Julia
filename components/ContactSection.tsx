d163e35699bdf8a29c28ccca6431ec9b23d3955a
BlakeSanderson44 <bsanderson96@gmail.com>
2025-10-12 14:32:06 -0700

    Ensure client components use double-quoted directive

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { ChangeEvent, FormEvent, useRef, useState } from 'react';
 
--- components/PropertiesSection.tsx
+++ components/PropertiesSection.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Image from 'next/image';
 import { defaultSizes, requireAlt } from '@/lib/image';
