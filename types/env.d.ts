8faec0e22453a633fb69f613bd62f1e5d9c5d427
BlakeSanderson44 <bsanderson96@gmail.com>
2025-10-12 14:10:49 -0700

    Add typed environment declarations

---
+++ types/env.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_HERO_IMAGE?: string;
+    // Add more typed envs here as needed:
+    // NEXT_PUBLIC_ANALYTICS_ID?: string;
+  }
+}
